f0b1c9c171890e755c9852cabcd921782cab7e72
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2024-12-23 10:54:46 -0300

    implement eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { loadEnv } from 'vite'
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import topics from 'starlight-sidebar-topics'
@@ -5,79 +6,84 @@ import sitemap from '@astrojs/sitemap'
 
 export const defaultLocale = 'pt-br'
 export const locales = {
-	'pt-br': { label: 'Português (BR)', lang: 'pt-BR' },
-	ja: { label: '日本語', lang: 'ja' },
-	en: { label: 'English', lang: 'en' }
+  'pt-br': { label: 'Português (BR)', lang: 'pt-BR' },
+  ja: { label: '日本語', lang: 'ja' },
+  en: { label: 'English', lang: 'en' }
 }
 
+const {
+  SITE: site
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
 export default defineConfig({
-	site: 'https://docs.evolved.com.br',
-	redirects: {
-		'/': '/pt-br/'
-	},
-	integrations: [
-		sitemap(),
-		starlight({
-			plugins: [
-				topics([
-					{
-						id: 'technical',
-						label: {
-							en: 'Technical',
-							'pt-BR': 'Técnico'
-						},
-						link: '/technical/',
-						icon: 'open-book',
-						items: ['technical/listed'],
-					},
-				])
-			],
-			title: 'Evolved Docs',
-			logo: {
-				src: './src/assets/logo.lr.web.png',
-				replacesTitle: true
-			},
-			customCss: ['./src/styles/custom.css'],
-			social: {
-				// azureDevOps: '',
-				// backstage: '',
-				// bitbucket: '',
-				// blueSky: '',
-				// codeberg: '',
-				codePen: 'https://codepen.io/nagaozen',
-				// discord: '',
-				// discourse: '',
-				// email: '',
-				// facebook: '',
-				github: 'https://github.com/nagaozen',
-				// gitlab: '',
-				// gitter: '',
-				// hackerOne: '',
-				instagram: 'https://www.instagram.com/nagaozen/',
-				linkedin: 'https://www.linkedin.com/in/nagaozen/',
-				// mastodon: '',
-				// matrix: '',
-				// microsoftTeams: '',
-				// nostr: '',
-				// openCollective: '',
-				// patreon: '',
-				// pinterest: '',
-				// reddit: '',
-				// rss: '',
-				// signal: '',
-				// slack: '',
-				// stackOverflow: '',
-				// telegram: '',
-				// threads: '',
-				// tiktok: '',
-				// twitch: '',
-				twitter: 'https://x.com/nagaozen',
-				'x.com': 'https://x.com/nagaozen',
-				// youtube: '',
-				// zulip: '',
-			},
-			defaultLocale,
-			locales
-		})
-	]
+  site,
+
+  redirects: {
+    '/': '/pt-br/'
+  },
+  integrations: [
+    sitemap(),
+    starlight({
+      plugins: [
+        topics([
+          {
+            id: 'technical',
+            label: {
+              en: 'Technical',
+              'pt-BR': 'Técnico'
+            },
+            link: '/technical/',
+            icon: 'open-book',
+            items: ['technical/listed'],
+          },
+        ])
+      ],
+      title: 'Evolved Docs',
+      logo: {
+        src: './src/assets/logo.lr.web.png',
+        replacesTitle: true
+      },
+      customCss: ['./src/styles/custom.css'],
+      social: {
+        // azureDevOps: '',
+        // backstage: '',
+        // bitbucket: '',
+        // blueSky: '',
+        // codeberg: '',
+        codePen: 'https://codepen.io/nagaozen',
+        // discord: '',
+        // discourse: '',
+        // email: '',
+        // facebook: '',
+        github: 'https://github.com/nagaozen',
+        // gitlab: '',
+        // gitter: '',
+        // hackerOne: '',
+        instagram: 'https://www.instagram.com/nagaozen/',
+        linkedin: 'https://www.linkedin.com/in/nagaozen/',
+        // mastodon: '',
+        // matrix: '',
+        // microsoftTeams: '',
+        // nostr: '',
+        // openCollective: '',
+        // patreon: '',
+        // pinterest: '',
+        // reddit: '',
+        // rss: '',
+        // signal: '',
+        // slack: '',
+        // stackOverflow: '',
+        // telegram: '',
+        // threads: '',
+        // tiktok: '',
+        // twitch: '',
+        twitter: 'https://x.com/nagaozen',
+        'x.com': 'https://x.com/nagaozen',
+        // youtube: '',
+        // zulip: '',
+      },
+      defaultLocale,
+      locales
+    })
+  ]
 })
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
 import { topicSchema } from 'starlight-sidebar-topics/schema'
 
 export const collections = {
-	docs: defineCollection({
-		schema: docsSchema({ extend: topicSchema })
-	}),
-	i18n: defineCollection({
-		type: 'data',
-		schema: i18nSchema()
-	})
+  docs: defineCollection({
+    schema: docsSchema({ extend: topicSchema })
+  }),
+  i18n: defineCollection({
+    type: 'data',
+    schema: i18nSchema()
+  })
 }
